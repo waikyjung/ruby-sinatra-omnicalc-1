89e57599a61d10aba7d7c1b998e19fc3bd588591
waikyjung <waiky.jung@gmail.com>
2024-10-18 02:38:54 +0000

    somewhat working

--- app.rb
+++ app.rb
@@ -2,8 +2,55 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square)
+end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+  
+get("/square/new/results") do
+  @cal_title = "Square"
+  @cal_type = "square"
+  @cal_path = "square"
+  @results = []
+  number = params.fetch("number").to_i
+  @results.push(["Number", number])
+  square = number ** 2.to_f
+  @results.push(["Square", square])
+  erb(:results)
+end
+
+get("/square_root/new/results") do
+  @cal_title = "Square Root"
+  @cal_type = "square root"
+  @cal_path = "square_root"
+  @results = []
+  number = params.fetch("number").to_i
+  @results.push(["Number", number])
+  square_root = number ** 0.5.to_f
+  @results.push(["Square Root", square_root])
+  erb(:results)
+end
+
+get("/payment/new/results") do
+  @cal_title = "Payment"
+  @cal_type = "payment"
+  @cal_path = "payment"
+  @results = []
+  number = params.fetch("apr")
+  @results.push(["APR", apr])
+  years = params.fetch("year").to_i
+  @results.push(["Number of years", years])
+  principal = params.fetch("principal").to_f.round(2)
+  @results.push(["Principal", principal])
+  erb(:results)
 end
